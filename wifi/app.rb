6c358d3cc1ffbeddef1823a0001d7a041ffe1bad
Marcus Schappi <marcus@schappi.com>
2012-05-29 07:50:16 +1000

    new bootstrap styled wifi config

--- wifi/app.rb
+++ wifi/app.rb
@@ -3,21 +3,6 @@
 require 'rubygems'
 require 'sinatra'
 
-#results = `iw wlan0 scan | grep -i 'ssid'`
-results = `cat scan.txt | grep -i ssid`
-
-@aps = results.split("\n");
-@aps.map! {|x| x.gsub('SSID: ','').strip }
-puts @aps.inspect
-
-#exit 0
-
 get '/' do
-  results = `cat scan.txt | grep -i ssid`
-
-  @aps = results.split("\n");
-  @aps.map! {|x| x.gsub('SSID: ','').strip }
-  puts @aps.inspect
-  erb  :index
-
+  erb :index
 end
