d4083ea5d6c19d3647b3fa906a7aaab327a3f763
Marcus Schappi <marcus@schappi.com>
2012-05-29 07:54:47 +1000

    added wifi logic code

--- wifi/app.rb
+++ wifi/app.rb
@@ -3,6 +3,21 @@
 require 'rubygems'
 require 'sinatra'
 
+@scanned = false
+@results = ""
+
+
 get '/' do
+  if scanned == false
+    @results = `iw wlan0 scan | grep -i 'ssid'`
+    @scanned = true  
+  else
+    @results = `iw wlan0 scan dump | grep -i 'ssid'`
+  end
+  
+  @aps = @results.split("\n");
+  @aps.map! {|x| x.gsub('SSID: ','').strip }
+  puts @aps.inspect
+  
   erb :index
 end
