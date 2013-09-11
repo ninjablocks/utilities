c2b299935bde3bcb33aba136f906beac89f0a81e
James Zaki <james.zaki@gmail.com>
2013-09-12 07:34:24 +1000

    Updated mjpg_start.sh with tip (comment) to increase camera resolution

--- bin/mjpg_start.sh
+++ bin/mjpg_start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 mjpg_streamer -i "/usr/local/lib/input_uvc.so -d /dev/video0" -o "/usr/local/lib/output_http.so -p 5000";
+
+# A higher resolution image can be taken if you modify the utilities script as suggested by Martn (sic)
+# on our forums http://forums.ninjablocks.com/index.php?p=/discussion/comment/6329#Comment_6329
+# For example modify above:
+#   mjpg_streamer -i "/usr/local/lib/input_uvc.so --resolution SXGA -d /dev/video0" \
+#      -o "/usr/local/lib/output_http.so -p 5000";
